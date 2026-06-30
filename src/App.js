63816dc1318b83c38e7be8e7b509117b012e4e48
Penke-Saivan <144597235+Penke-Saivan@users.noreply.github.com>
2026-06-30 15:04:25 +0530

    Update API URL in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = 'https://testtestest-execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
+  const apiUrl = 'https://q5xoqlmefk.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
